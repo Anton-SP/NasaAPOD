631ced2431ac219263fcab265a218448703c019d
AntonSP <toha1988cool@gmail.com>
2022-05-01 09:43:07 +0300

    HomeWork1 init

--- app/src/main/java/com/example/nasaapod/MainActivity.kt
+++ app/src/main/java/com/example/nasaapod/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.nasaapod
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * it has begun
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

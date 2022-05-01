3924923d58750f25c6f195250a8995352f0aefc6
AntonSP <toha1988cool@gmail.com>
2022-05-01 23:33:52 +0300

    HomeWork1 dev basic load

---
+++ app/src/main/java/com/example/nasaapod/domain/NasaRepositoryImp.kt
@@ -0,0 +1,25 @@
+package com.example.nasaapod.domain
+
+import com.example.nasaapod.api.ApodResponse
+import com.example.nasaapod.api.NasaApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private val api = Retrofit.Builder()
+    .addConverterFactory(GsonConverterFactory.create())
+    .baseUrl("https://api.nasa.gov/")
+    .client(OkHttpClient.Builder().apply {
+        addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+    }
+        .build()
+    )
+    .build()
+    .create(NasaApi::class.java)
+
+class NasaRepositoryImp:NasaRepository {
+    override suspend fun Apod(): ApodResponse = api.getAPOD("GQr3kQbJTcVYc72YNrRg8GCNKbT6s618nfnS5COB")
+}
